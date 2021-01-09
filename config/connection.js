71937651ca3f68235d926768168f306dc07e3616
Elan Zinkow <ezinkow@gmail.com>
2021-01-09 14:41:43 -0600

    fix text html

--- config/connection.js
+++ config/connection.js
@@ -1,18 +1,5 @@
 var mysql = require("mysql");
-// var express = require("express");
-// var exphbs = require("express-handlebars");
-// var app = express();
 
-// var PORT = process.env.PORT || 8080;
-
-// app.use(express.static("public"));
-
-// // Sets up the Express app to handle data parsing
-// app.use(express.urlencoded({ extended: true }));
-// app.use(express.json());
-
-// app.engine("handlebars", exphbs({ defaultLayout: "main" }));
-// app.set("view engine", "handlebars");
 var connection;
 
 if(process.env.JAWSDB_URL){
@@ -34,10 +21,6 @@ connection.connect(function(err) {
   }
   console.log("connected as id " + connection.threadId);
 
-//   app.listen(PORT, function() {
-//     // Log (server-side) when our server has started
-//     console.log("Server listening on: http://localhost:" + PORT);
-//   });
 });
 
 module.exports = connection;
